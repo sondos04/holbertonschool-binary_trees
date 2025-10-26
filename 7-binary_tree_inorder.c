8351f8857c704a2f3baf9cfc2707d57541d3f91b
sondos04 <sondos.alrubaih04@gmail.com>
2025-10-26 12:13:16 +0000

    edit Betty documentation style

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -2,7 +2,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - Goes through a binary tree using in-order traversal
+ * binary_tree_inorder - Goes through a binary tree using in-order traversal
  * @tree: Pointer to the root node of the tree to traverse
  * @func: Pointer to a function to call for each node (takes an int)
  *

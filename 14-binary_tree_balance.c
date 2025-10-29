7ef9a782438e7c08ad4ec45893a7de212488e03d
root <root@ip-10-42-151-164.ec2.internal>
2025-10-29 11:04:41 +0000

    fix(task 14): Betty style + docs; reuse binary_tree_height

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,29 +1,24 @@
 #include "binary_trees.h"
 
-/* Helper: height in number of nodes on the longest path */
-static size_t height(const binary_tree_t *tree)
-{
-    size_t hl, hr;
-
-    if (tree == NULL)
-        return 0;
-
-    hl = height(tree->left);
-    hr = height(tree->right);
-    return (hl >= hr ? hl : hr) + 1;
-}
-
 /**
- * binary_tree_balance - measures the balance factor of a binary tree
- * @tree: pointer to the root node of the tree to measure the balance factor
+ * binary_tree_balance - Measures the balance factor of a binary tree.
+ * @tree: Pointer to the root node of the tree to measure the balance factor.
  *
- * Return: balance factor (left height - right height),
- *         or 0 if tree is NULL.
+ * Return: The balance factor (left height - right height).
+ *         If @tree is NULL, return (0).
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return 0;
+	size_t lh = 0, rh = 0;
 
-    return (int)height(tree->left) - (int)height(tree->right);
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left != NULL)
+		lh = 1 + binary_tree_height(tree->left);
+	if (tree->right != NULL)
+		rh = 1 + binary_tree_height(tree->right);
+
+	return ((int)(lh - rh));
 }
+

b95e18f47b7ffec3592570f23774b2749fa37504
root <root@ip-10-42-151-164.ec2.internal>
2025-10-29 11:22:23 +0000

    task 14: balance factor using local edge-height helper (Betty ok)

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,22 +1,22 @@
 #include "binary_trees.h"
 
 /**
- * height - Measures the height of a binary tree (in edges).
- * @tree: Pointer to the node to measure from.
+ * height_edges - Return height of a tree in edges (local helper).
+ * @t: Pointer to node.
  *
- * Return: Height (0 for NULL or leaf); uses edges as the unit.
+ * Return: Height in edges (0 for a single node), 0 for NULL.
  */
-static size_t height(const binary_tree_t *tree)
+static size_t height_edges(const binary_tree_t *t)
 {
 	size_t lh = 0, rh = 0;
 
-	if (tree == NULL)
+	if (!t)
 		return (0);
 
-	if (tree->left != NULL)
-		lh = 1 + height(tree->left);
-	if (tree->right != NULL)
-		rh = 1 + height(tree->right);
+	if (t->left)
+		lh = 1 + height_edges(t->left);
+	if (t->right)
+		rh = 1 + height_edges(t->right);
 
 	return (lh > rh ? lh : rh);
 }
@@ -25,15 +25,13 @@ static size_t height(const binary_tree_t *tree)
  * binary_tree_balance - Measures the balance factor of a binary tree.
  * @tree: Pointer to the root node of the tree to measure.
  *
- * Return: The balance factor (left height - right height).
- *         If @tree is NULL, return (0).
+ * Return: left-height - right-height. If @tree is NULL, return 0.
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	if (tree == NULL)
+	if (!tree)
 		return (0);
 
-	return ((int)height(tree->left) - (int)height(tree->right));
-}
+	return ((int)height_edges(tree->left) - (int)height_edges(tree->right));
 }
 
